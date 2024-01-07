c789c8f81efcb1c14772af3157449413b4c4cd4e
lang0502 <153420127+lang0502@users.noreply.github.com>
2024-01-07 22:19:57 +0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+var popupconfirm = confirm("Jika Menggunakan Web HandPhone , Miringkan Layar HP Ada Biar Tampilan Full dan Rapi")
+if(popupconfirm === true)
+{
+       alert("Abaikan JIka Menggunakan Browser Laptop/Computer");
+}
+else{
+       alert("Maaf Jika Tampilan tidak Sempurna");
+
+}
+
+
