2202935afc191eb08b8ed0597da1d74bc325f175
lang0502 <153420127+lang0502@users.noreply.github.com>
2024-01-08 12:09:11 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var popupconfirm = confirm("Jika Menggunakan Web HandPhone , Miringkan Layar HP Ada Biar Tampilan Full dan Rapi")
+var popupconfirm = confirm("Jika Menggunakan Web HandPhone , Miringkan Layar HP Anda Biar Tampilan Full dan Rapi")
 if(popupconfirm === true)
 {
        alert("Abaikan JIka Menggunakan Browser Laptop/Computer");
